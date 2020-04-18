13b3bf6a2aa05c060ce641cf9ebc925486723f80
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-04-18 17:37:02 +1200

    users can now add their own options

--- src/components/planner-comps/Options.jsx
+++ src/components/planner-comps/Options.jsx
@@ -1,24 +1,50 @@
-import React, { Fragment } from "react"
+import React, { Fragment, Component } from "react"
 
 import DragBox from './DragBox'
 
 
-const options = {
+let options = {
   physical: ["physical 1", "physical 2"],
   mental: ["mental 1", "mental 2"],
   spiritual: ["spiritual 1", "spiritual 2"],
   social: ["social 1", "social 2"]
 }
 
-function Options(props) {
-  const { pillar } = props
-  const currentOptions = options[pillar]
+class Options extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activity: ""
+    }
+  }
 
-  return (
-    <Fragment>
-      {currentOptions.map((option, i) => { return <DragBox text={option} key={i}/> })}
-    </Fragment>
-  )
+  handleChange = (e) => {
+    this.setState({
+      activity: e.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    console.log(this.props.pillar, this.state.activity)
+    options[this.props.pillar].push(this.state.activity)
+    this.setState({
+      activity: ""
+    })
+  }
+
+  render() {
+    const { pillar } = this.props
+    const { activity } = this.state
+    const currentOptions = options[pillar]
+
+    return (
+      <Fragment>
+        <input type="text" placeholder="Add Your Own..." value={activity} onChange={this.handleChange}/>
+        <button onClick={this.handleSubmit}>+</button>
+        {currentOptions.map((option, i) => { return <DragBox text={option} key={i} /> })}
+      </Fragment>
+    )
+  }
 }
 
 export default Options
