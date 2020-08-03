50f75b1af2a0bad28def6acc8827fd0136012ad6
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-08-03 13:37:46 +1200

    chosen color palette

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,13 @@ import { Link } from 'gatsby'
 function Header() {
   return (
     <div className="nav-bar">
-      <h1 id="main-title">Hauora</h1>
+      <h1 id="main-title">Hauora Planner</h1>
       <div className="nav-links">
-        <Link to="/profile/" activeClassName="active-link">User Profile</Link>
-        <Link to="/dashboard/" activeClassName="active-link">Dashboard</Link>
-        <Link to="/planner/" activeClassName="active-link">Weekly Planner</Link>
-        <Link to="/stats/" activeClassName="active-link">Stats/Progress</Link>
+        {/* <Link to="/profile/" activeClassName="active-link">User Profile</Link> */}
+        {/* <Link to="/dashboard/" activeClassName="active-link">Dashboard</Link> */}
+        {/* <Link to="/planner/" activeClassName="active-link">Weekly Planner</Link> */}
+        {/* <Link to="/stats/" activeClassName="active-link">Stats/Progress</Link> */}
+        <Link to="/"><button>Sign Out!</button></Link>
       </div>
     </div>
   )
