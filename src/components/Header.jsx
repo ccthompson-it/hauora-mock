e8b54f9de67b3ea7e578e2dbad287e36f21ca589
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-09-07 19:19:14 +1200

    added custom page title and 404 page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ import { Link } from 'gatsby'
 
 function Header() {
   return (
-    <div className="header">
-      <h1 id="main-title">Hauora Planner</h1>
-      <div className="nav-bar">
-        <Link to="/profile/" className="nav-button" activeClassName="active-link">User Profile</Link>
-        <Link to="/dashboard/" className="nav-button" activeClassName="active-link">Dashboard</Link>
-        <Link to="/planner/" className="nav-button" activeClassName="active-link">Weekly Planner</Link>
-        <Link to="/stats/" className="nav-button" activeClassName="active-link">Stats/Progress</Link>
-        <Link to="/" className="nav-button">Sign Out</Link>
+    <React.Fragment>
+      <div className="header">
+        <h1 id="main-title">Hauora Planner</h1>
+        <div className="nav-bar">
+          <Link to="/profile/" className="nav-button" activeClassName="active-link">User Profile</Link>
+          <Link to="/dashboard/" className="nav-button" activeClassName="active-link">Dashboard</Link>
+          <Link to="/planner/" className="nav-button" activeClassName="active-link">Weekly Planner</Link>
+          <Link to="/stats/" className="nav-button" activeClassName="active-link">Stats/Progress</Link>
+          <Link to="/" className="nav-button">Sign Out</Link>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
 
